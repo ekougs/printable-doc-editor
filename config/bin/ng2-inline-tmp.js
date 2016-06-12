7b3a937f32fbfc5b7d53c2f23b1b46a47dcbbafd
ekougs <ekougs@gmail.com>
2016-06-12 14:03:17 +0000

    #19 HTML inline fix

---
+++ config/bin/ng2-inline-tmp.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+(() => {
+	'use strict';
+	var fs = require('fs');
+	var path = require('path');
+	var mkdirp = require('mkdirp').sync;
+	var minimist = require('minimist');
+	var glob = require('glob');
+	var inliner = require('angular2-inline-template-style');
+	var flattener = require('angular2-inline-template-style/bin/flattener');
+	var upper = require('angular2-inline-template-style/bin/upper');
+	var chokidar = require('chokidar');
+
+	var args = minimist(process.argv.slice(2), {
+		alias: {
+			u: 'up',
+			f: 'flatten',
+			o: 'outDir',
+			b: 'base',
+			c: 'compress',
+			w: 'watch'
+		},
+		string: ['outDir', 'base'],
+		boolean: ['flatten', 'compress', 'watch'],
+		number: ['up']
+	});
+
+	var globPath = args._.slice()[0];
+	if (!globPath || typeof globPath === 'undefined' || globPath.length === 0) {
+		doUsage();
+	} else if (args.watch) {
+		doWatch(globPath, args);
+	} else {
+		doOnce(globPath, args);
+	}
+
+	function doWatch(globPath, args) {
+		chokidar.watch(globPath).on('all', (event, path) => {
+			console.log(event, path);
+			doOnce(path, args);
+		});
+	}
+
+	function doOnce(globPath, args) {
+		glob(globPath, {}, (er, files) => {
+			if (er) {
+				console.error('failed to find files for path: ' + globPath);
+				process.exit(1);
+			} else {
+				files.forEach((file) => {
+        			console.error("path", file);
+					var target = path.join(process.cwd(), file);
+					var content = fs.readFileSync(target);
+					if (content) {
+						content = inliner(content.toString(), {
+							base: args.base,
+							compress: args.compress
+						}, path.dirname(target));
+
+						if (args.flatten) {
+							file = flattener.flatten(file);
+						} else if (typeof args.up === 'number') {
+							file = upper(file, args.up);
+						}
+
+						var destination = path.join(args.outDir, file);
+						if (!fs.existsSync(path.dirname(destination))) {
+							mkdirp(path.dirname(destination));
+						}
+
+						fs.writeFile(destination, content, err => {
+							if (err) {
+								console.error('error processing file: ' + file + ', produced error: ' + err);
+							}
+						});
+					} else {
+						console.error('failed to read target: ' + target);
+					}
+				});
+			}
+		});
+	}
+
+	function doUsage() {
+		console.log('ng2-inline [--outDir|-o] [--base|-b] [--flatten|-f] [--up|-u <count>] <path glob>');
+	}
+})();
